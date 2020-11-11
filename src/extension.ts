ed2f64b20fb46cab1865a34bfe0b7376abd72974
Ilkka Poutanen <ilkka@ilkka.dev>
2020-11-11 16:26:15 +0200

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+/*
+ * Copyright 2020 Ilkka Poutanen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import * as vscode from "vscode";
 import * as cp from "child_process";
 
